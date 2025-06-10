fc434e2ca1b3b134d7cf2c251ef416cb8ab543d8
Pimenova, Marina <mnfpma@gmail.com>
2025-06-10 11:34:30 +0300

    Init repository

---
+++ src/test/java/com/training/classicalragapi/ClassicalRagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.classicalragapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicalRagApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
